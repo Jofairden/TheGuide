035fbe513866ef75fc931f0e7c243f4e327a91ae
Jofairden <danielzondervan.nl@gmail.com>
2017-04-07 16:39:51 +0200

    Made SoftGreen color more green

--- Helpers.cs
+++ Helpers.cs
@@ -19,7 +19,7 @@ namespace TheGuide
 			public static Color Blue => new Color(0f, 0f, 1f);
 			public static Color Yellow => new Color(1f, 1f, 0f);
 			public static Color SoftRed => new Color((byte)242, 152, 140);
-			public static Color SoftGreen => new Color((byte)184, 242, 140);
+			public static Color SoftGreen => new Color((byte)140, 242, 140);
 			public static Color SoftYellow => new Color((byte)242, 235, 140);
 
 			public static Color GetLatencyColor(float latency, float offset = 0f) =>
